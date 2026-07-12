a713e37b9af1f285c010666a3e08cf61ae6b8096
James Tran <j4mestran@gmail.com>
2026-07-12 23:45:26 +0100

    10.12

--- learning/series10.jl
+++ learning/series10.jl
@@ -6,4 +6,40 @@ println("Hello World!")
 1 == 1 || 2 > 3
 true + true * 2
 
+x = 1
+y = 2
+z = x + y
+
+x = 2
+
+y
+
+z
+
+z  = x + y
+
+i = 1
+i += 1
+
+typeof(-3)
+typeof(3.14)
+typeof(pi)
+typeof("Hello World!")
+round(pi; digits = 2)
+1000000 == 1_000_000
+
+div(4, 2)
+4 ÷ 2
+4 / 2
+
+
+typeof('a')
+typeof('a' + 1)
+typeof("char")
+
+s3 = "doggo"
+println("$s3 is a good boy!")
+
+s1 = "Hello"
+s1_s3 = s1 * s3
 
