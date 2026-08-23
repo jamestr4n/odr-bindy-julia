50bdb5e96556d8a5cf70abb638dbe0ca7a2e0fc3
jamestr4n <j4mestran@gmail.com>
2026-08-23 21:38:10 +0100

    Example: Lorenz using DataDrivenSparse

--- learning/DataDrivenDiffEq/datadriven.jl
+++ learning/DataDrivenDiffEq/datadriven.jl
@@ -3,3 +3,43 @@ using ModelingToolkit
 using OrdinaryDiffEq
 using DataDrivenSparse
 using LinearAlgebra
+using Plots
+
+function lorenz(u, p, t)
+    x, y, z = u
+
+    ẋ = 10.0 * (y - x)
+    ẏ = x * (28.0 - z) - y
+    ż = x * y - (8 / 3) * z
+    return [ẋ, ẏ, ż]
+end
+
+u0 = [1.0; 0.0; 0.0]
+tspan = (0.0, 100.0)
+dt = 0.1
+prob = ODEProblem(lorenz, u0, tspan)
+sol = solve(prob, Tsit5(), saveat = dt)
+
+## Start the automatic discovery
+ddprob = DataDrivenProblem(sol)
+
+@variables t x(t) y(t) z(t)
+u = [x; y; z]
+basis = Basis(polynomial_basis(u, 5), u, iv = t)
+opt = STLSQ(exp10.(-5:0.1:-1))
+ddsol = solve(ddprob, basis, opt, options = DataDrivenCommonOptions(digits = 1))
+println(get_basis(ddsol))
+
+#Plot
+plot(
+    plot(sol, label = string.([:x :y :z]), title = "Simulated (sol)"),
+    plot(ddprob, title = "Sampled data (ddprob)"),
+    layout = (1, 2)
+)
+plot(ddsol)
+
+plot(
+    plot(sol, label = string.([:x :y :z]), title = "actual"),
+    plot(ddsol, title = "predicted"),
+    layout = (2, 1)
+)
